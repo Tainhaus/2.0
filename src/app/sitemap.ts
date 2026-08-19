90650eeed67089af886037b6e34b497e2805162e
Tainhaus <nicholasstewartfountain@gmail.com>
2026-08-19 10:01:44 +0100

    Fix: update sitemap base URL to www.tainhaus.co.uk

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://tainhaus.co.uk";
+  const baseUrl = "https://www.tainhaus.co.uk";
   const now = new Date();
 
   return [
