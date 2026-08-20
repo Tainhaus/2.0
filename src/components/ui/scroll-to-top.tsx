1a40df8230018caa4f6f46e3a91841b72c7b9d91
Tainhaus <nicholasstewartfountain@gmail.com>
2026-08-20 08:11:29 +0100

    Add: ScrollToTop client component

---
+++ src/components/ui/scroll-to-top.tsx
@@ -0,0 +1,11 @@
+// src/components/ui/scroll-to-top.tsx
+"use client";
+
+import { useEffect } from "react";
+
+export function ScrollToTop() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "instant" });
+  }, []);
+  return null;
+}
